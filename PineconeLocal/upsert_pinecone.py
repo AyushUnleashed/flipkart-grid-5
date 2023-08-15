5dbf29cdc4c631d224ee976529889156acfeba73
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-08-15 21:45:25 +0530

    Changed column names in upsert_pinceone

--- PineconeLocal/upsert_pinecone.py
+++ PineconeLocal/upsert_pinecone.py
@@ -12,7 +12,7 @@ def get_images(data):
     for index, row in data.iterrows():
         try:
             # Download image from the 'link' column
-            image_url = row['styleImage']
+            image_url = row['style_image']
             response = requests.get(image_url)
 
             if response.status_code == 200:
@@ -32,11 +32,11 @@ def insert_data(pinecone_index, model, bm25, data, images, batch_size=200):
             data_batch = data.iloc[i:i_end]
 
             # change in columns being considered for meta_dict
-            meta_dict = data_batch[['id', 'productDisplayName', 'brandName', 'masterCategory', 'subCategory', 'articleType', 'gender', 'baseColour', 'season', 'Occasion', 'isJewellery', 'styleImage']].to_dict(orient="records")
+            meta_dict = data_batch[['id', 'product_display_name', 'brand_name', 'master_category', 'sub_category', 'article_type', 'gender', 'color', 'season', 'occasion', 'is_jewellery', 'style_image']].to_dict(orient="records")
             print(data.columns)
 
             # narrowed down columns that need to be converted to strings and checked for 'none'
-            cols_to_consider = ['productDisplayName', 'masterCategory', 'subCategory', 'baseColour', 'Pattern', 'Occasion', 'Sleeve styling', 'Sleeve length', 'Fabric', 'Neck']
+            cols_to_consider = ['product_display_name', 'master_category', 'sub_category', 'color', 'pattern', 'occasion', 'sleeve_styling', 'sleeve_length', 'fabric', 'neck']
             cols_for_pinecone_query = data_batch[cols_to_consider]
             pinecone_query_string = [ " ".join(str(val) for col, val in row.items() if val != 'none') for _, row in cols_for_pinecone_query.iterrows()]
             img_batch = images[i:i_end]
@@ -61,7 +61,7 @@ def insert_data(pinecone_index, model, bm25, data, images, batch_size=200):
 def upsert_csv(csv_file,char_enc, pinecone_index, model, bm25):
     try:
         data = pd.read_csv(csv_file, encoding=char_enc)
-        data = data[['id', 'productDisplayName', 'brandName', 'baseColour','masterCategory', 'subCategory', 'articleType', 'gender', 'baseColour', 'season', 'Occasion', 'isJewellery', 'styleImage', 'Pattern', 'Occasion', 'Sleeve styling', 'Sleeve length', 'Fabric', 'Neck']] # use only specified columns
+        data = data[['id', 'product_display_name', 'brand_name', 'color', 'master_category', 'sub_category', 'article_type', 'gender', 'season', 'occasion', 'is_jewellery', 'style_image', 'pattern', 'sleeve_styling', 'sleeve_length', 'fabric', 'neck']] # use only specified columns
         images = get_images(data)
         # fit the bm25 model with the 'productDisplayName' column
         bm25.fit(data['productDisplayName'])
