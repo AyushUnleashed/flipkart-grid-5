b194190d732f693bafc6b6cd1a2373c164576c18
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-18 00:51:39 +0530

    Some basic styling of the front page

--- frontend/neytiri-frontend/src/App.js
+++ frontend/neytiri-frontend/src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import broom from "./broom.png";
+import chatIcon from "./chatIcon.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="Container"></div>
+      <div className="Container">
+        <p class="WelcomeText">
+          Welcome to Neytiri: An outfit Avatar Generator!
+        </p>
+      </div>
       <div className="QueryHolder">
         <button className="NewButton">
           {" "}
@@ -13,13 +18,16 @@ function App() {
           Search!
         </button>
         <form>
-          <input
-            className="PromptText"
-            placeholder="What the outfit you want?"
-            type="text"
-            id="fname"
-            name="fname"
-          />
+          <div className="ChatContainer">
+            <img src={chatIcon} className="ChatImage" />
+            <input
+              className="PromptText"
+              placeholder="What the outfit you want?"
+              type="text"
+              id="fname"
+              name="fname"
+            />
+          </div>
         </form>
       </div>
     </div>
