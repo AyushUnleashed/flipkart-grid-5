e58555fb1cb7319921d20508d47b02d2f9dd106c
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-20 07:00:37 +0530

    Jhumka vector embedding preperared, Loading state spinner added

--- frontend/neytiri-frontend/src/App.js
+++ frontend/neytiri-frontend/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import { useEffect, useRef, useState } from "react";
 import OutfitCard from "./OutfitCard";
 import UserMessage from "./UserMessage";
+import LoadingSpinner from "./Loader/Loader";
 
 function App() {
   // var countMessages = 0;
@@ -16,217 +17,217 @@ function App() {
   const [placeholderText, setPlaceholderText] = useState(
     "What outfit are you looking for?"
   );
-  // const [chatLog, setChatLog] = useState([
-  //   {
-  //     IsUser: true,
-  //     UserPrompt: "give me tradional outfit",
-  //   },
-  //   {
-  //     IsUser: false,
-  //     OutFits: [
-  //       {
-  //         article_type: "tshirts",
-  //         brand_name: "hanes",
-  //         color: "black",
-  //         gender: "men",
-  //         id: 49963,
-  //         is_jewellery: false,
-  //         master_category: "apparel",
-  //         occasion: "casual",
-  //         product_display_name: "hanes men black pack of 3 t-shirt",
-  //         season: "summer",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/Hanes-Men-Black-Pack-of-3-T-shirt_424f9c9fcdaa10ff2b4fe6b1fa6c18bd_images.jpg",
-  //         sub_category: "topwear",
-  //       },
-  //       {
-  //         article_type: "shorts",
-  //         brand_name: "hanes",
-  //         color: "black",
-  //         gender: "men",
-  //         id: 49958,
-  //         is_jewellery: false,
-  //         master_category: "apparel",
-  //         occasion: "casual",
-  //         product_display_name: "hanes men black athletic 3/4th pants",
-  //         season: "summer",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/Hanes-Men-Black-34-Length-Pants_086779a69d95e440744e02cbb33e1581_images.jpg",
-  //         sub_category: "bottomwear",
-  //       },
-  //       {
-  //         article_type: "casual_shoes",
-  //         brand_name: "converse",
-  //         color: "white",
-  //         gender: "unisex",
-  //         id: 4441,
-  //         is_jewellery: false,
-  //         master_category: "footwear",
-  //         occasion: "none",
-  //         product_display_name: "converse unisex canvas hi white shoe",
-  //         season: "fall",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/e6c6697fd3aabe8522060801baf0e711_images.jpg",
-  //         sub_category: "shoes",
-  //       },
-  //       {
-  //         article_type: "watches",
-  //         brand_name: "esprit",
-  //         color: "steel",
-  //         gender: "men",
-  //         id: 10237,
-  //         is_jewellery: false,
-  //         master_category: "accessories",
-  //         occasion: "none",
-  //         product_display_name: "esprit men croso black steel watch",
-  //         season: "winter",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/cf1675179772f0380d5c44bb797e7df1_images.jpg",
-  //         sub_category: "watches",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     IsUser: true,
-  //     UserPrompt:
-  //       "give me tradional outfit in the topwear, ethinic in bottomwear, indian in footwear and accessories",
-  //   },
-  //   {
-  //     IsUser: false,
-  //     OutFits: [
-  //       {
-  //         article_type: "kurtas",
-  //         brand_name: "fabindia",
-  //         color: "red",
-  //         gender: "men",
-  //         id: 56691,
-  //         is_jewellery: false,
-  //         master_category: "apparel",
-  //         occasion: "none",
-  //         product_display_name: "fabindia men red kurta",
-  //         season: "summer",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/Fabindia-Men-Red-Kurta_3cca499449f3df92bca89ccf710216a8_images.jpg",
-  //         sub_category: "topwear",
-  //       },
-  //       {
-  //         article_type: "shorts",
-  //         brand_name: "adidas",
-  //         color: "white",
-  //         gender: "men",
-  //         id: 2192,
-  //         is_jewellery: false,
-  //         master_category: "apparel",
-  //         occasion: "none",
-  //         product_display_name: "adidas men white short",
-  //         season: "fall",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/d811aea0d123b18f15fa2a07e134998a_images.jpg",
-  //         sub_category: "bottomwear",
-  //       },
-  //       {
-  //         article_type: "sandals",
-  //         brand_name: "ganuchi",
-  //         color: "brown",
-  //         gender: "men",
-  //         id: 11937,
-  //         is_jewellery: false,
-  //         master_category: "footwear",
-  //         occasion: "none",
-  //         product_display_name: "ganuchi men casual brown sandals",
-  //         season: "fall",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/785b8d7da04f23c999c1f424f7c73547_images.jpg",
-  //         sub_category: "sandal",
-  //       },
-  //       {
-  //         article_type: "ties",
-  //         brand_name: "park_avenue",
-  //         color: "blue",
-  //         gender: "men",
-  //         id: 49744,
-  //         is_jewellery: false,
-  //         master_category: "accessories",
-  //         occasion: "none",
-  //         product_display_name: "park avenue men blue tie",
-  //         season: "summer",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/a1436f3c892dc55886386d535f73a4ac_images.jpg",
-  //         sub_category: "ties",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     IsUser: true,
-  //     UserPrompt: "bottom wear is boring, give me ethinic ",
-  //   },
-  //   {
-  //     IsUser: false,
-  //     OutFits: [
-  //       {
-  //         article_type: "kurtas",
-  //         brand_name: "fabindia",
-  //         color: "red",
-  //         gender: "men",
-  //         id: 56691,
-  //         is_jewellery: false,
-  //         master_category: "apparel",
-  //         occasion: "none",
-  //         product_display_name: "fabindia men red kurta",
-  //         season: "summer",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/Fabindia-Men-Red-Kurta_3cca499449f3df92bca89ccf710216a8_images.jpg",
-  //         sub_category: "topwear",
-  //       },
-  //       {
-  //         article_type: "trousers",
-  //         brand_name: "mark_taylor",
-  //         color: "black",
-  //         gender: "men",
-  //         id: 27180,
-  //         is_jewellery: false,
-  //         master_category: "apparel",
-  //         occasion: "none",
-  //         product_display_name: "mark taylor men striped black trousers",
-  //         season: "summer",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/dcc262870d3b0b66b6c806b18fa3d1bd_images.jpg",
-  //         sub_category: "bottomwear",
-  //       },
-  //       {
-  //         article_type: "sandals",
-  //         brand_name: "ganuchi",
-  //         color: "brown",
-  //         gender: "men",
-  //         id: 11937,
-  //         is_jewellery: false,
-  //         master_category: "footwear",
-  //         occasion: "none",
-  //         product_display_name: "ganuchi men casual brown sandals",
-  //         season: "fall",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/785b8d7da04f23c999c1f424f7c73547_images.jpg",
-  //         sub_category: "sandal",
-  //       },
-  //       {
-  //         article_type: "ties",
-  //         brand_name: "park_avenue",
-  //         color: "blue",
-  //         gender: "men",
-  //         id: 49744,
-  //         is_jewellery: false,
-  //         master_category: "accessories",
-  //         occasion: "none",
-  //         product_display_name: "park avenue men blue tie",
-  //         season: "summer",
-  //         style_image:
-  //           "http://assets.myntassets.com/v1/images/style/properties/a1436f3c892dc55886386d535f73a4ac_images.jpg",
-  //         sub_category: "ties",
-  //       },
-  //     ],
-  //   },
-  // ]);
-  const [chatLog, setChatLog] = useState([]);
+  const [chatLog, setChatLog] = useState([
+    {
+      IsUser: true,
+      UserPrompt: "give me tradional outfit",
+    },
+    {
+      IsUser: false,
+      OutFits: [
+        {
+          article_type: "tshirts",
+          brand_name: "hanes",
+          color: "black",
+          gender: "men",
+          id: 49963,
+          is_jewellery: false,
+          master_category: "apparel",
+          occasion: "casual",
+          product_display_name: "hanes men black pack of 3 t-shirt",
+          season: "summer",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/Hanes-Men-Black-Pack-of-3-T-shirt_424f9c9fcdaa10ff2b4fe6b1fa6c18bd_images.jpg",
+          sub_category: "topwear",
+        },
+        {
+          article_type: "shorts",
+          brand_name: "hanes",
+          color: "black",
+          gender: "men",
+          id: 49958,
+          is_jewellery: false,
+          master_category: "apparel",
+          occasion: "casual",
+          product_display_name: "hanes men black athletic 3/4th pants",
+          season: "summer",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/Hanes-Men-Black-34-Length-Pants_086779a69d95e440744e02cbb33e1581_images.jpg",
+          sub_category: "bottomwear",
+        },
+        {
+          article_type: "casual_shoes",
+          brand_name: "converse",
+          color: "white",
+          gender: "unisex",
+          id: 4441,
+          is_jewellery: false,
+          master_category: "footwear",
+          occasion: "none",
+          product_display_name: "converse unisex canvas hi white shoe",
+          season: "fall",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/e6c6697fd3aabe8522060801baf0e711_images.jpg",
+          sub_category: "shoes",
+        },
+        {
+          article_type: "watches",
+          brand_name: "esprit",
+          color: "steel",
+          gender: "men",
+          id: 10237,
+          is_jewellery: false,
+          master_category: "accessories",
+          occasion: "none",
+          product_display_name: "esprit men croso black steel watch",
+          season: "winter",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/cf1675179772f0380d5c44bb797e7df1_images.jpg",
+          sub_category: "watches",
+        },
+      ],
+    },
+    {
+      IsUser: true,
+      UserPrompt:
+        "give me tradional outfit in the topwear, ethinic in bottomwear, indian in footwear and accessories",
+    },
+    {
+      IsUser: false,
+      OutFits: [
+        {
+          article_type: "kurtas",
+          brand_name: "fabindia",
+          color: "red",
+          gender: "men",
+          id: 56691,
+          is_jewellery: false,
+          master_category: "apparel",
+          occasion: "none",
+          product_display_name: "fabindia men red kurta",
+          season: "summer",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/Fabindia-Men-Red-Kurta_3cca499449f3df92bca89ccf710216a8_images.jpg",
+          sub_category: "topwear",
+        },
+        {
+          article_type: "shorts",
+          brand_name: "adidas",
+          color: "white",
+          gender: "men",
+          id: 2192,
+          is_jewellery: false,
+          master_category: "apparel",
+          occasion: "none",
+          product_display_name: "adidas men white short",
+          season: "fall",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/d811aea0d123b18f15fa2a07e134998a_images.jpg",
+          sub_category: "bottomwear",
+        },
+        {
+          article_type: "sandals",
+          brand_name: "ganuchi",
+          color: "brown",
+          gender: "men",
+          id: 11937,
+          is_jewellery: false,
+          master_category: "footwear",
+          occasion: "none",
+          product_display_name: "ganuchi men casual brown sandals",
+          season: "fall",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/785b8d7da04f23c999c1f424f7c73547_images.jpg",
+          sub_category: "sandal",
+        },
+        {
+          article_type: "ties",
+          brand_name: "park_avenue",
+          color: "blue",
+          gender: "men",
+          id: 49744,
+          is_jewellery: false,
+          master_category: "accessories",
+          occasion: "none",
+          product_display_name: "park avenue men blue tie",
+          season: "summer",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/a1436f3c892dc55886386d535f73a4ac_images.jpg",
+          sub_category: "ties",
+        },
+      ],
+    },
+    {
+      IsUser: true,
+      UserPrompt: "bottom wear is boring, give me ethinic ",
+    },
+    {
+      IsUser: false,
+      OutFits: [
+        {
+          article_type: "kurtas",
+          brand_name: "fabindia",
+          color: "red",
+          gender: "men",
+          id: 56691,
+          is_jewellery: false,
+          master_category: "apparel",
+          occasion: "none",
+          product_display_name: "fabindia men red kurta",
+          season: "summer",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/Fabindia-Men-Red-Kurta_3cca499449f3df92bca89ccf710216a8_images.jpg",
+          sub_category: "topwear",
+        },
+        {
+          article_type: "trousers",
+          brand_name: "mark_taylor",
+          color: "black",
+          gender: "men",
+          id: 27180,
+          is_jewellery: false,
+          master_category: "apparel",
+          occasion: "none",
+          product_display_name: "mark taylor men striped black trousers",
+          season: "summer",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/dcc262870d3b0b66b6c806b18fa3d1bd_images.jpg",
+          sub_category: "bottomwear",
+        },
+        {
+          article_type: "sandals",
+          brand_name: "ganuchi",
+          color: "brown",
+          gender: "men",
+          id: 11937,
+          is_jewellery: false,
+          master_category: "footwear",
+          occasion: "none",
+          product_display_name: "ganuchi men casual brown sandals",
+          season: "fall",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/785b8d7da04f23c999c1f424f7c73547_images.jpg",
+          sub_category: "sandal",
+        },
+        {
+          article_type: "ties",
+          brand_name: "park_avenue",
+          color: "blue",
+          gender: "men",
+          id: 49744,
+          is_jewellery: false,
+          master_category: "accessories",
+          occasion: "none",
+          product_display_name: "park avenue men blue tie",
+          season: "summer",
+          style_image:
+            "http://assets.myntassets.com/v1/images/style/properties/a1436f3c892dc55886386d535f73a4ac_images.jpg",
+          sub_category: "ties",
+        },
+      ],
+    },
+  ]);
+  // const [chatLog, setChatLog] = useState([]);
 
   useEffect(() => {
     window.addEventListener("beforeunload", clearChat);
@@ -521,9 +522,7 @@ function App() {
   };
   return (
     <div ref={messagesContainerRef} className="App">
-      <h2 className="WelcomeText">
-        Welcome to Neytiri: An outfit Avatar Generator!
-      </h2>
+      <h2 className="WelcomeText">Neytiri: A GenAI Outfit Generator!</h2>
       <hr className="HorizotalLine" />
       <div className="Container ">
         {chatLog.map((Obj, index) => (
@@ -546,6 +545,7 @@ function App() {
                       <div className="Card2">
                         {" "}
                         Waiting for the response, Hold Up!
+                        {isSubmitDisabled ? <LoadingSpinner /> : null}
                       </div>
                     )}
                   </>
